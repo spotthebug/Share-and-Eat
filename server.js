33ff3d0bf46507061f2701a17232c8735f12a027
spotthebug <gjain1@scu.edu>
2018-04-10 23:52:27 -0700

    Create login and signup views and routes.

--- server.js
+++ server.js
@@ -76,6 +76,44 @@ app.get("/", function(req, res) {
 
 });
 
+// Signup
+app.get('/signup', function (req, res) {
+ res.render('signup');
+});
+
+
+app.post("/signup", function (req, res) {
+  console.log("sanity check!! pre-signup");
+  User.register(new User({ name: req.body.username, email: req.body.email}), req.body.password,
+      function (err, newUser) {
+        console.log("Check if it enter function to auth");
+        console.log("ERROR", err);
+        console.log("NEW USER!!",newUser);
+        passport.authenticate("local")(req, res, function() {
+          res.redirect("/");
+        });
+      }
+  );
+});
+
+// Login and Logout
+
+app.post('/login',passport.authenticate('local'), function (req, res){
+  res.redirect("/");
+});
+
+app.get('/login', function(req, res){
+  res.render("login")
+});
+
+app.get('/logout', function (req, res){
+  console.log("Before logout", JSON.stringify(req.user));
+  req.logout();
+  console.log("After logout", JSON.stringify(req.user));
+  res.redirect('/')
+});
+
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("listening..");
