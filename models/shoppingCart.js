c265fc1c264d98d9f19c746a79e99699af167351
spotthebug <gjain1@scu.edu>
2018-04-12 14:49:01 -0700

    Refined nav options for login and signup and added the nav as well.

--- models/shoppingCart.js
+++ models/shoppingCart.js
@@ -4,8 +4,8 @@ Schema = mongoose.Schema;
 var shoppingCartSchema = new Schema({
   item: [{type: Schema.Types.ObjectId, ref:'MenuItem'}],
   // user: {type: Schema.Types.ObjectId, ref:'User'},
-  quantity: {type: Number, required: true, default: 0}
-  // total: Number
+  quantity: {type: Number, required: true, default: 0},
+  total: Number
 });
 
 var shoppingCart = mongoose.model('shoppingCart', shoppingCartSchema);
